6a80bf8a6146dfe725f41752ca87413fc79d9a7a
Yuval Shimon <yuvalshi0@gmail.com>
2020-05-23 17:34:29 +0300

    changed file structure to work on moblie

---
+++ sort-visual/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Graph from "./Graph";
+import * as serviceWorker from "./serviceWorker";
+
+function App() {
+  return <Graph />;
+};
+
+export default App;
